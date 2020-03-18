c8fde4b4b2132d881bf35858c51cbbb76edcaf4c
Pharia Le <pharia.le@gmail.com>
2020-03-18 07:55:40 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,20 +2,9 @@ class User
   
   attr_accessor :first_name, :last_name
   
-  @@all = []
-  
   def initialize
     @first_name = first_name
     @last_name = last_name
-    self.save
-  end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.all
-    @@all
   end
   
 end
